0e761b7e9bca3965a7dc2ae3485d73dbe3806f76
brunomerlo <brunomerlo2602@gmail.com>
2025-05-20 17:21:26 -0300

    Update Camino.java

--- src/logica/Camino.java
+++ src/logica/Camino.java
@@ -1,4 +1,33 @@
 package logica;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Camino {
+	
+	private List<Celda> celdas;
+	
+	public Camino() {
+		celdas = new ArrayList<>();
+	}
+
+	public Camino(Camino otro) {
+		celdas = new ArrayList<>(otro.celdas);  // Copia superficial
+	}
+
+	public void agregarCelda(Celda celda) {
+		celdas.add(celda);
+	}
+
+	public int getTamaño() {
+		return celdas.size();
+	}
+
+	public void eliminarCelda(int i) {
+		celdas.remove(i);
+	}
+
+	public Celda getCelda(int i) {
+		return celdas.get(i);
+	}
 }
