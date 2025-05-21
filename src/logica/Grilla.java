179b1a2a1f5114d751fa460aa67b506b47a79ac7
brunomerlo <brunomerlo2602@gmail.com>
2025-05-20 21:34:21 -0300

    Update Grilla.java

--- src/logica/Grilla.java
+++ src/logica/Grilla.java
@@ -1,21 +1,28 @@
 package logica;
 
+import java.util.Random;
+
 public class Grilla {
 	private Celda[][] matriz;
 	private int filas;
 	private int columnas;
 
-	public Grilla(boolean[][] cargas) {
-		this.filas = cargas.length;
-		this.columnas = cargas[0].length;
-		this.matriz = new Celda[filas][columnas];
-
-		for (int i = 0; i < filas; i++) {
-			for (int j = 0; j < columnas; j++) {
-				matriz[i][j] = new Celda(i, j, cargas[i][j]);
-			}
-		}
+	public Grilla(int filas, int columnas) {
+	    this.filas = filas;
+	    this.columnas = columnas;
+	    this.matriz = new Celda[filas][columnas];
 	}
+
+	public void generarGrillaAleatoria() {
+	    Random rand = new Random();
+	    for (int fila = 0; fila < filas; fila++) {
+	        for (int col = 0; col < columnas; col++) {
+	            boolean carga = rand.nextBoolean();
+	            matriz[fila][col] = new Celda(fila, col, carga);
+	        }
+	    }
+	}
+
 
 	public Celda getCelda(int fila, int columna) {
 		return matriz[fila][columna];
@@ -28,5 +35,6 @@ public class Grilla {
 	public int getColumnas() {
 		return columnas;
 	}
-	
+
+
 }
