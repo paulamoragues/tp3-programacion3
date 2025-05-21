4c861022c2389789f0a30860be270c6b60fda6a5
brunomerlo <brunomerlo2602@gmail.com>
2025-05-20 21:32:24 -0300

    Update FuerzaBruta.java

--- src/logica/FuerzaBruta.java
+++ src/logica/FuerzaBruta.java
@@ -6,36 +6,40 @@ import java.util.List;
 public class FuerzaBruta {
 
 	private List<Camino> caminosValidosSinPoda;
-	
-
 	private Camino caminoActual;
 	private Grilla grilla;
 	private int filas;
 	private int columnas;
-
 	// Estadisticas
-	private int llamadasSinPoda;
-	
+	private int llamadasSinPoda;	
 
+
+	
 	public FuerzaBruta(Grilla grilla) {
 		this.grilla = grilla;
 		this.filas = grilla.getFilas();
 		this.columnas = grilla.getColumnas();
 		this.caminosValidosSinPoda = new ArrayList<>();
 		this.caminoActual= new Camino();		
-		this.llamadasSinPoda = 0;
+		this.llamadasSinPoda = 0;	
+	}
+
 	
-	}
-
 	public List<Camino> buscarCaminosMinimosSinPoda() {
+	    // Limpiar resultados y reiniciar contador antes de cada búsqueda
+	    caminosValidosSinPoda.clear();
+	    llamadasSinPoda = 0;
+	    caminoActual = new Camino();
 
-		Celda inicio = grilla.getCelda(0, 0);
-		caminoActual.agregarCelda(inicio);
+	    Celda inicio = grilla.getCelda(0, 0);
+	    caminoActual.agregarCelda(inicio);
 
-		buscarSinPoda(0, 0, cargaComoEntero(inicio));
-
-		return caminosValidosSinPoda;
+	    buscarSinPoda(0, 0, cargaComoEntero(inicio));
+	
+	    return caminosValidosSinPoda;
 	}
+
+
 
 	private void buscarSinPoda(int fila, int columna, int suma) {
 		llamadasSinPoda++;
@@ -66,7 +70,6 @@ public class FuerzaBruta {
 			caminoActual.eliminarCelda(caminoActual.getTamaño() - 1);
 		}
 	}
-
 	
 
 	public boolean llegoAlDestino(int fila, int columna) {
@@ -74,8 +77,10 @@ public class FuerzaBruta {
 	}
 
 	private int cargaComoEntero(Celda celda) {
-		return celda.getCarga() ? 1 : -1;
+	  
+	    return celda.getCarga() ? 1 : -1;
 	}
+
 
 	// para ver en el main
 	public int getLlamadasSinPoda() {
@@ -96,7 +101,6 @@ public class FuerzaBruta {
 			System.out.println("\n-----");
 		}
 	}
-
 
 
 
