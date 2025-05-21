8a7f0d56f2cbd6c8de326e545eeececb1bb960bc
brunomerlo <brunomerlo2602@gmail.com>
2025-05-20 21:30:42 -0300

    Update Main.java

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -1,44 +1,49 @@
 package interfaz;
 
-import logica.BackTracking;
-import logica.FuerzaBruta;
-import logica.Grilla;
+import javax.swing.SwingUtilities;
+
+//import logica.BackTracking;
+//import logica.FuerzaBruta;
+//import logica.Grilla;
 
 public class Main {
-
-	// provisorio
+//
+//	// provisorio
+//	public static void main(String[] args) {
+//		int filas = 6;
+//		int columnas = 5;
+//		Grilla grilla = new Grilla(filas, columnas);
+//		grilla.generarGrillaAleatoria();
+//		FuerzaBruta algoritmoSinPoda = new FuerzaBruta(grilla);
+//
+//		// Estadisticas
+//		long inicioSinPoda = System.currentTimeMillis();
+//		algoritmoSinPoda.buscarCaminosMinimosSinPoda();
+//		long finSinPoda = System.currentTimeMillis();
+//		
+//		System.out.println("Caminos encontrados sin poda: " + algoritmoSinPoda.getCantidadCaminosSinPoda());
+//		System.out.println("Llamadas recursivas sin poda: " + algoritmoSinPoda.getLlamadasSinPoda());
+//		System.out.println("Tiempo de ejecución sin poda: " + (finSinPoda - inicioSinPoda) + " ms");
+//		algoritmoSinPoda.imprimirCaminosSinPoda();
+//		
+//		BackTracking algoritmoConPoda = new BackTracking(grilla);
+//		long inicioConPoda = System.currentTimeMillis();
+//		algoritmoConPoda.buscarCaminosMinimosConPoda();
+//		long finConPoda = System.currentTimeMillis();
+//
+//
+//
+//		System.out.println("Caminos encontrados con poda: " + algoritmoConPoda.getCantidadCaminosConPoda());
+//		System.out.println("Llamadas recursivas con poda: " + algoritmoConPoda.getLlamadasConPoda());
+//		System.out.println("Tiempo de ejecución con poda: " + (finConPoda - inicioConPoda) + " ms");
+//		algoritmoConPoda.imprimirCaminosConPoda();
+//		
+//	}
 	public static void main(String[] args) {
-		boolean[][] cargas = { 
-				{ true, false, false, false }, 
-				{ false, true, true, false },
-				{ true, true, true, false }, 
-		};
-
-		Grilla grilla = new Grilla(cargas);
-		FuerzaBruta algoritmoSinPoda = new FuerzaBruta(grilla);
-
-		// Estadisticas
-		long inicioSinPoda = System.currentTimeMillis();
-		algoritmoSinPoda.buscarCaminosMinimosSinPoda();
-		long finSinPoda = System.currentTimeMillis();
-		
-		System.out.println("Caminos encontrados sin poda: " + algoritmoSinPoda.getCantidadCaminosSinPoda());
-		System.out.println("Llamadas recursivas sin poda: " + algoritmoSinPoda.getLlamadasSinPoda());
-		System.out.println("Tiempo de ejecución sin poda: " + (finSinPoda - inicioSinPoda) + " ms");
-		algoritmoSinPoda.imprimirCaminosSinPoda();
-		
-		BackTracking algoritmoConPoda = new BackTracking(grilla);
-		long inicioConPoda = System.currentTimeMillis();
-		algoritmoConPoda.buscarCaminosMinimosConPoda();
-		long finConPoda = System.currentTimeMillis();
-
-
-
-		System.out.println("Caminos encontrados con poda: " + algoritmoConPoda.getCantidadCaminosConPoda());
-		System.out.println("Llamadas recursivas con poda: " + algoritmoConPoda.getLlamadasConPoda());
-		System.out.println("Tiempo de ejecución con poda: " + (finConPoda - inicioConPoda) + " ms");
-		algoritmoConPoda.imprimirCaminosConPoda();
-		
+		SwingUtilities.invokeLater(() -> {
+			Pantalla pantalla = new Pantalla();
+			pantalla.setVisible(true);
+		});
 	}
 
 }
