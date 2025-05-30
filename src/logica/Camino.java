ee55d0042b1e19f4ce9a4a8f1414643bbb2cdd08
brunomerlo <brunomerlo2602@gmail.com>
2025-05-30 16:24:51 -0300

    Update Camino.java

--- src/logica/Camino.java
+++ src/logica/Camino.java
@@ -2,6 +2,7 @@ package logica;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Camino {
 	private List<Celda> celdas;
@@ -43,26 +44,51 @@ public class Camino {
 	}
 
 	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Camino camino = (Camino) o;
-		// Dos caminos son iguales si tienen las mismas celdas en el mismo orden y
-		// tamaño
-		if (this.celdas.size() != camino.celdas.size()) {
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (obj == null || getClass() != obj.getClass()) return false;
+		Camino otro = (Camino) obj;
+		
+		if (this.celdas.size() != otro.celdas.size()) {
 			return false;
 		}
 		for (int i = 0; i < this.celdas.size(); i++) {
 			Celda c1 = this.celdas.get(i);
-			Celda c2 = camino.celdas.get(i);
-			// Compara las celdas por su posición (fila, columna), no por la carga
-			// directamente
-			if (c1.getFila() != c2.getFila() || c1.getColumna() != c2.getColumna()) {
+			Celda c2 = otro.celdas.get(i);
+		
+			if (!c1.equals(c2)) { 
 				return false;
 			}
 		}
 		return true;
+	}
+	
+	@Override
+    public int hashCode() {
+        return Objects.hash(celdas); // Genera el hashcode basado en la lista de celdas
+    }
+	public boolean esCaminoValido(Grilla grilla) {
+		if (celdas.isEmpty()) {
+			return false;
+		}
+
+		Celda ultimaCelda = celdas.get(getTamaño() - 1);
+		
+		// 1. Verificar si llega al destino
+		boolean llegoAlDestino = (ultimaCelda.getFila() == grilla.getFilas() - 1) &&
+								 (ultimaCelda.getColumna() == grilla.getColumnas() - 1);
+		
+		// 2. Verificar la suma de cargas
+		boolean sumaCorrecta = (calcularSumaCargas() == 0);
+		
+		return llegoAlDestino && sumaCorrecta;
+	}
+
+	public int calcularSumaCargas() {
+		int suma = 0;
+		for (Celda celda : celdas) {
+			suma += (celda.getCarga() ? 1 : -1);
+		}
+		return suma;
 	}
 }
